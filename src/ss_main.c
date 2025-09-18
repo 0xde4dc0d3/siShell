5d590b67f46464e0a4605f22bd66a65f2d18270b
ecrasevv <francesco.ostuni001@studenti.uniparthenope.it>
2025-09-18 14:28:46 +0200

    moved all in src/

---
+++ src/ss_main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "ss_init.h"
+#include "ss_core.h"
+
+int main(int argc, const char **argv) {
+    // Shell startup
+    SS_INFO ss_info;
+    ss_init(&ss_info);
+    // Loop
+    ss_loop(ss_info);
+    // Exit 
+    exit(EXIT_SUCCESS);
+}
---
+++ src/ss_visual.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <pwd.h>
+#include "ss_visual.h"
+
+/** 
+    * @brief display siShell prompt
+*/
+void ss_display_prompt(SS_INFO ss_info) {
+    printf(RED "[" RESET);
+    printf("%s@%s ", ss_info.pw->pw_name, ss_info.hostname);
+    printf(BLUE "%s" RESET, ss_info.cwd_name);
+    printf(RED "]" RESET);
+    printf("$ ");
+}
