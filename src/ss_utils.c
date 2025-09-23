1ca84b488cb8aa663d115d9fb81f09f902ebe491
ecrasevv <francesco.ostuni001@studenti.uniparthenope.it>
2025-09-23 23:12:55 +0200

    implemented levenshtein distance to find the best match between a wrong command and a ss_builtin command

--- src/ss_utils.c
+++ src/ss_utils.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "ss_utils.h"
+#include "ss_builtin.h"
 
 char *ss_get_cwd_name(SS_INFO ss_info) {
     int idx = (int)strlen(ss_info.abs_cwd);
@@ -31,4 +32,60 @@ char *ss_read_line(void) {
         }
     }
     return stdin_line;
+}
+
+int levenshtein_distance(const char *str_1, const char *str_2) {
+    const int len_1 = strlen(str_1)+1;
+    const int len_2 = strlen(str_2)+1;
+
+    int **matrix = (int**)calloc(len_1, sizeof(int*));
+    if (!matrix) {
+        perror("matrix allocation error");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < len_1; ++i) {
+        int *vec = (int*)calloc(len_2, sizeof(int));
+        if (!vec) {
+            perror("vector allocation error");
+            exit(EXIT_FAILURE);
+        }
+        matrix[i] = vec;
+    }
+
+    for (int i = 0; i < len_1; ++i)
+        matrix[i][0] = i;
+
+    for (int j = 0; j < len_2; ++j)
+        matrix[0][j] = j;
+
+    for (int i = 1; i < len_1; ++i) {
+        for (int j = 1; j < len_2; ++j) {
+            int subcost = (str_1[i-1] == str_2[j-1]) ? 0 : 1;
+            int ij_cost = MIN(matrix[i-1][j]+1, matrix[i][j-1]+1);
+            ij_cost = MIN(ij_cost, matrix[i-1][j-1]+subcost);
+            matrix[i][j] = ij_cost;
+        }
+    }
+
+    int distance = matrix[len_1-1][len_2-1];
+    
+    for (int i = 0; i < len_1; ++i)
+        free(matrix[i]);
+    free(matrix);
+
+    return distance;
+}
+
+int get_closest_one(const char *str) {
+    int idx = 0;
+    int distance = 999999;
+    for (int i = 0; i < SS_BUILTIN_LEN; ++i) {
+        int new_distance = levenshtein_distance(str, ss_builtin[i]);
+        if (new_distance < distance) {
+            distance = new_distance;
+            idx = i;
+        }
+    }
+    return idx;
 }
--- src/ss_utils.h
+++ src/ss_utils.h
@@ -5,17 +5,34 @@
 
 #define SS_STDIN_LINE_LEN 128
 
+#define MIN(a, b) ((a < b)?(a):(b))
+
 /**
     * @brief retrive only the name of the current dir, used in ss_display_prompt
     * @param ss_info the struct containing all the siShell info
     FIXME: I think all this can be done in the while loop, I'm too lazy now
- */
+*/
 char *ss_get_cwd_name(SS_INFO ss_info);
 
 /**
     * @brief read user input from stdin 
     * @return char* a string containig user commmand or NULL
- */ 
+*/ 
 char *ss_read_line(void);
+
+/**
+    * @brief levenshtein_distance used to find the best match between two commands
+    * @param str_1 first command 
+    * @param str_2 second command
+    * @return distance number of editing operation to transform str_1 in str_2
+*/ 
+int levenshtein_distance(const char *str_1, const char *str_2);
+
+/**
+    * @brief find the command in ss_builtin that is most similar to the one entered by the user
+    * @param str the command entered by the user
+    * @return idx of the best match in ss_builtin
+*/ 
+int get_closest_one(const char *str);
 
 #endif // !SS_UTILS_H
